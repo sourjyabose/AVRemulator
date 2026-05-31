6b3d551a1c145ca5f47dbac91c043c32acf49e43
Sourjya Basu <sourjyabose@gmail.com>
2026-05-31 19:45:41 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main() {
+
+    DDRB |= (1 << PB0);
+
+    while(1) {
+
+        PORTB ^= (1 << PB0);
+
+        _delay_ms(500);
+    }
+}
---
+++ reader.py
@@ -0,0 +1,21 @@
+class Instruc:
+    def __init__(self,name,mask,pattern,handler):
+        self.name=name;
+        self.mask=mask;
+        self.pattern=pattern;
+        self.handler=handler;
+
+def rjump(p):
+    print("rjump",p);
+
+InstrucTable=[Instruc("rjump",0b1111000000000000,0b1100000000000000,rjump)]
+
+file=open("main.bin","rb")
+content=file.read()
+i=0;
+low=0b0;
+high=0b0;
+pc=0;
+while pc<len(content):
+    
+

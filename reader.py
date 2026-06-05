3b4421e897937f3194d0bca922e94731c43cbb61
Sourjya Basu <sourjyabose@gmail.com>
2026-06-05 10:29:59 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -10,13 +10,17 @@ class memory:
         self.mem=[0]*(159-95)
     def __getitem__(self,key):
         if key>=69 and key<=159:
-            print("")
+            print("Accessing Memory",key)
             return self.mem[key-69]
         
 
 class io:
     def __init__(self):
-        self.ioaddr=[0]*(95-31);
+        self.__ioaddr=[0]*(95-31);
+    def __getitem__(self,key,value):
+        return self.__ioaddr[key]
+    def __setitem__(self,key,value):
+        self.__ioaddr[key]=value;
 
 
 class Instruc:
@@ -27,7 +31,7 @@ class Instruc:
         self.handler=handler;
 
 cpu=stateMachine();
-
+io_mem=io();
 def rjump(p):
     res=p&0b0000111111111111;
     res*=2;
@@ -39,16 +43,24 @@ def eor(p):
     destReg=(p&0b0000000111110000)>>4
     srcReg=(p&0b0000000000001111)|((p&0b0000001000000000)>>5)
     cpu.regfile[destReg]=cpu.regfile[destReg]^cpu.regfile[srcReg]
-    print("EOR","R"+str(destReg)+" <- R"+str(srcReg)); 
+    print("EOR","R"+str(destReg)+" ^ R"+str(srcReg));
+
 
 def out(p):
     srcReg=(p&0b0000000111110000)>>4
     ioport=(p&0b0000000000001111)|((p&0b0000011000000000)>>5)
-    print("OUT");
+    io_mem[ioport]=cpu.regfile[srcReg];
+    print("OUT",f"R{srcReg} ->",hex(ioport));
+
+def ldi(p):
+    reg=(p&0b0000000111110000)>>4;
+    constant=()
+    pass
 
 InstrucTable=[Instruc("rjump",0b1111000000000000,0b1100000000000000,rjump),
               Instruc("eor",0b1111110000000000,0b0010010000000000,eor),
-              Instruc("out",0b1111100000000000,0b1011100000000000,out)]
+              Instruc("out",0b1111100000000000,0b1011100000000000,out),
+              Instruc("ldi",0b1111000000000000,0b1110000000000000,ldi)]
 
 file=open("main.bin","rb")
 content=file.read()
